d9080192216aeda78962d278c85fcd8b4fd567eb
Tom Chauvin <tchauvin@seeyousun.fr>
2025-05-16 16:23:28 +0200

    Moyenne

--- tests/test_streamlit_app.py
+++ tests/test_streamlit_app.py
@@ -1,5 +1,6 @@
 from sysprod.streamlit_app import strip_accents
 from sysprod.streamlit_app import calcul_p90_mensuel
+from sysprod.streamlit_app import calcul_moyenne_mensuelle
 from pytest import mark, raises
 import warnings
 from pytest import approx
@@ -35,9 +36,18 @@ def test_calcul_p90_mensuel():
     p90_met_month, p90_pvgis_month = calcul_p90_mensuel(met, pvgis, p50_met, p90_met, p50_pvgis, p90_pvgis)
 
     print("\nP90 MET mensuel :", p90_met_month)
-    print("P90 PVGIS mensuel :", p90_pvgis_month)
+    print("\nP90 PVGIS mensuel :", p90_pvgis_month)
 
     assert p90_met_month == approx([3.12 ,4.58, 8.13, 11.35, 12.87, 13.76, 13.80, 12.08, 9.77, 5.90, 3.76, 2.74 ])
     assert p90_pvgis_month == approx([3.39, 5.96, 7.27, 12.85, 11.80, 14.07, 13.49, 13.64, 10.78, 5.97, 4.14, 3.18])
 
-    
+
+def test_calcul_moyenne_mensuelle():
+    p90_met_month = [3.12, 4.58, 8.13, 11.35, 12.87, 13.76, 13.80, 12.08, 9.77, 5.90, 3.76, 2.74]
+    p90_pvgis_month = [3.39, 5.96, 7.27, 12.85, 11.80, 14.07, 13.49, 13.64, 10.78, 5.97, 4.14, 3.18]
+
+    moyenne = calcul_moyenne_mensuelle(p90_met_month, p90_pvgis_month)
+    print("Production en P90 (moyenne MET & PVGIS) :", moyenne)
+
+    assert moyenne == approx([3.252, 5.274, 7.7, 12.099, 12.336, 13.913, 13.643, 12.86, 10.275, 5.935, 3.95, 2.96], abs=0.01)
+
